9d545c2ad62d584c159b365bcb35196be039b12b
unknown <guilherme.laudecarvalho@gmail.com>
2023-06-22 22:48:34 -0300

    commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="app">
+      <h1 className="text-3xl  underline font-bold">
+      Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
